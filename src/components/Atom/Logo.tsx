5db3d2865a09f4dc28def2480307f7b374d9f2b8
jieun-c <flffl1231@naver.com>
2023-05-19 22:08:58 +0900

    로고 이미지 경로 수정

--- src/components/Atom/Logo.tsx
+++ src/components/Atom/Logo.tsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <>
       <Link to="/" className="block h-full">
-        <img src="/images/logo.svg" alt="logo" className="h-full" />
+        <img src="/flori/images/logo.svg" alt="logo" className="h-full" />
       </Link>
     </>
   );
